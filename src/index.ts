321d19706bd7a01f461f166b449c49070ca9a15d
Dejan Vucic <dejan.vucic.ibdev@gmail.com>
2023-12-02 09:25:08 +0100

    Added ticker and infinite run animation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-import { Application, Text, Graphics, BitmapText, BitmapFont, ParticleContainer, Texture } from 'pixi.js'
+import { Application, Graphics, ParticleContainer, Texture } from 'pixi.js'
 import { AdvancedBloomFilter } from 'pixi-filters'
 import { Emitter, upgradeConfig } from '@pixi/particle-emitter'
 import * as particleSettings from '../static/particles/sunParticles.json'
 import { Scene } from './scenes/Scene'
 
 const sunFilter = new AdvancedBloomFilter({ bloomScale: 3, blur: 15, quality: 13 })
-// const solaireFilter = new AdvancedBloomFilter({ bloomScale: 0.3, blur: 1 })
 const particleContainer = new ParticleContainer()
 
 const app = new Application({
@@ -16,10 +15,8 @@ const app = new Application({
 	width: 1920,
 	height: 1080,
 })
-// globalThis.__PIXI_APP__ = app
 
-const sceny: Scene = new Scene()
-sceny.position.set(0, app.screen.height / 2)
+const sceny: Scene = new Scene(app.screen.width, app.screen.height)
 
 const sun: Graphics = new Graphics()
 sun.beginFill('0xffbe6f')
@@ -28,43 +25,10 @@ sun.endFill()
 sun.angle = 30
 sun.position.set(app.screen.width - 100, 50)
 
-const label: Text = new Text('Solaire of Astora', {
-	fontStyle: 'oblique',
-	fontWeight: '500',
-	fontSize: 24,
-	fontFamily: 'Verdana',
-	align: 'center',
-	fill: ['#ff5c0a', '#c64600'],
-})
-label.position.set(0, -30)
-
-BitmapFont.from(
-	'Verdana',
-	{
-		fill: '#ffffff',
-		fontFamily: 'Verdana',
-		fontSize: 20,
-	},
-	{ chars: BitmapFont.ASCII }
-)
-
-const quote: BitmapText = new BitmapText('Praise The Sun!', {
-	fontSize: 20,
-	fontName: 'Verdana',
-	align: 'left',
-	tint: 0xc64600,
-})
-
 const emitter = new Emitter(particleContainer, upgradeConfig(particleSettings, Texture.from('images/particle.png')))
 emitter.autoUpdate = true
 emitter.updateSpawnPos(200, 0)
 emitter.emit = true
 
-quote.text = 'If only I could be so grossly incandescent!'
-quote.position.set(0, 400)
-
 app.stage.addChild(sceny, sun)
 sun.filters = [sunFilter]
-// app.stage.addChild(particleContainer)
-// sceny.solaire.addChild(label, quote)
-// sceny.solaire.filters = [solaireFilter]
--- src/scenes/Scene.ts
+++ src/scenes/Scene.ts
@@ -1,29 +1,51 @@
-import { AnimatedSprite, Container, Spritesheet, BaseTexture } from 'pixi.js'
-import runAnimationData from '../../static/images/dodgeAnimation.json'
+import { AnimatedSprite, Container, Spritesheet, BaseTexture, Ticker } from 'pixi.js'
+import dodgeAnimationData from '../../static/images/runAnimation.json'
 
 export class Scene extends Container {
-	constructor() {
-		super()
+	private readonly screenWidth: number
+	private readonly screenHeight: number
 
+	private knight!: AnimatedSprite
+	private readonly knightSpeed: number = 6
+	private goingRight: boolean = true
+
+	constructor(screenWidth: number, screenHeight: number) {
+		super()
+		this.screenWidth = screenWidth
+		this.screenHeight = screenHeight
 		this.initialize()
 	}
 
 	private async initialize(): Promise<void> {
-		const sheet = new Spritesheet(BaseTexture.from(runAnimationData.meta.image), runAnimationData)
+		const sheet = new Spritesheet(BaseTexture.from(dodgeAnimationData.meta.image), dodgeAnimationData)
 		await sheet.parse()
-		const animatedKnight = new AnimatedSprite(sheet.animations['dodge'])
-		this.addChild(animatedKnight)
-		animatedKnight.anchor.set(0.5)
-		animatedKnight.onFrameChange = this.onKnightFrameChange.bind(this)
+		this.knight = new AnimatedSprite(sheet.animations['run'])
+		this.addChild(this.knight)
+		this.knight.anchor.set(0.5)
+		this.knight.position.y = this.screenHeight - 170
+		this.knight.position.x = 0
+		this.knight.play()
+		this.knight.animationSpeed = 0.5
+		this.knight.loop = true
 
-		animatedKnight.animationSpeed = 0.55
-		animatedKnight.play()
-		setTimeout(() => {
-			animatedKnight.stop()
-		}, 5500)
+		Ticker.shared.add(this.onKnightFrameChange.bind(this))
 	}
 
-	private onKnightFrameChange(): void {
-		this.position.x = this.position.x + 10
+	private onKnightFrameChange(deltatime: number): void {
+		if (this.goingRight) {
+			this.goRight(deltatime)
+			this.knight.scale.x = 1
+		} else {
+			this.goLeft(deltatime)
+			this.knight.scale.x = -1
+		}
+	}
+	private goRight(deltatime: number): void {
+		this.knight.position.x = this.knight.position.x + this.knightSpeed * deltatime
+		if (this.knight.position.x >= this.screenWidth - 50) this.goingRight = false
+	}
+	private goLeft(deltatime: number): void {
+		this.knight.position.x = this.knight.position.x - this.knightSpeed * deltatime
+		if (this.knight.position.x <= 0) this.goingRight = true
 	}
 }
